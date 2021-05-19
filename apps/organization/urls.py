e51430ef23c60bfc041c7cfe709e8b495782105c
XrazYang <xrazyang@gmail.com>
2021-05-19 11:45:29 +0800

    机构页面功能完善

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -6,10 +6,15 @@
 
 from django.urls import path
 from django.conf.urls import url
-from apps.organization.views import OrgView, AddAskView
+from apps.organization.views import OrgView, AddAskView, OrgHomeView, OrgTeacherView, OrgCourseView, OrgDescView
 
 urlpatterns = [
     # path('org_list/', OrgView.as_view(), name='org_list'),
     url(r'^list/$', OrgView.as_view(), name='list'),
-    url(r'^add_ask/$', AddAskView.as_view(), name='add_ask')
+    url(r'^add_ask/$', AddAskView.as_view(), name='add_ask'),
+    url(r'^(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='org_home'),
+    url(r'^(?P<org_id>\d+)/teacher/$', OrgTeacherView.as_view(), name='org_teacher'),
+    url(r'^(?P<org_id>\d+)/course/$', OrgCourseView.as_view(), name='org_course'),
+    url(r'^(?P<org_id>\d+)/desc/$', OrgDescView.as_view(), name='org_desc'),
+    # path('<int:org_id>/', OrgHomeView.as_view(), name='org_home'),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -7,6 +7,103 @@ from pure_pagination import Paginator, PageNotAnInteger
 from apps.organization.models import CourseOrg, City
 from apps.organization.forms import AddAskForm
 
+from apps.operation.models import UserFavorite
+
+
+class OrgDescView(View):
+    def get(self, request, org_id, *args, **kwargs):
+        current_page = "org_desc"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        course_org.click_nums += 1
+        course_org.save()
+
+        # has_fav = False
+        # if request.user.is_authenticated:
+        #     if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+        #         has_fav = True
+
+        return render(request, "org-detail-desc.html", {
+            "course_org": course_org,
+            "current_page": current_page,
+            # "has_fav": has_fav
+        })
+
+
+class OrgCourseView(View):
+    def get(self, request, org_id, *args, **kwargs):
+        current_page = "org_course"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        course_org.click_nums += 1
+        course_org.save()
+
+        all_courses = course_org.course_set.all()
+
+        # has_fav = False
+        # if request.user.is_authenticated:
+        #     if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+        #         has_fav = True
+
+        # 对课程机构数据进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_courses, per_page=1, request=request)
+        courses = p.page(page)
+
+        return render(request, "org-detail-course.html", {
+            "all_courses": courses,
+            "course_org": course_org,
+            "current_page": current_page,
+            # "has_fav":has_fav
+        })
+
+
+class OrgTeacherView(View):
+    def get(self, request, org_id, *args, **kwargs):
+        current_page = "org_teacher"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        course_org.click_nums += 1
+        course_org.save()
+
+        # has_fav = False
+        # if request.user.is_authenticated:
+        #     if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+        #         has_fav = True
+
+        all_teachers = course_org.teacher_set.all()
+        return render(request, "org-detail-teachers.html", {
+            "all_teachers": all_teachers,
+            "course_org": course_org,
+            "current_page": current_page,
+            # "has_fav": has_fav
+        })
+
+
+class OrgHomeView(View):
+    def get(self, request, org_id, *args, **kwargs):
+        current_page = "org_home"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        course_org.click_nums += 1
+        course_org.save()
+
+        # has_fav = False
+        # if request.user.is_authenticated:
+        #     if UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+        #         has_fav = True
+
+        all_courses = course_org.course_set.all()[:3]
+        all_teachers = course_org.teacher_set.all()[:1]
+
+        return render(request, "org-detail-homepage.html", {
+            "all_courses": all_courses,
+            "all_teachers": all_teachers,
+            "course_org": course_org,
+            "current_page": current_page,
+            # "has_fav": has_fav
+        })
+
 
 class AddAskView(View):
     def post(self, request, *args, **kwargs):
