10e5fe90b1fde7e11d8ab9e42bb0085e74835fe8
XrazYang <xrazyang@gmail.com>
2021-05-26 16:10:21 +0800

    跳转修改

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -6,7 +6,8 @@
 
 from django.urls import path
 from django.conf.urls import url
-from apps.organization.views import OrgView, AddAskView, OrgHomeView, OrgTeacherView, OrgCourseView, OrgDescView
+from apps.organization.views import OrgView, AddAskView, OrgHomeView, OrgTeacherView, OrgCourseView, OrgDescView, \
+    TeacherListView
 
 urlpatterns = [
     # path('org_list/', OrgView.as_view(), name='org_list'),
@@ -16,5 +17,7 @@ urlpatterns = [
     url(r'^(?P<org_id>\d+)/teacher/$', OrgTeacherView.as_view(), name='org_teacher'),
     url(r'^(?P<org_id>\d+)/course/$', OrgCourseView.as_view(), name='org_course'),
     url(r'^(?P<org_id>\d+)/desc/$', OrgDescView.as_view(), name='org_desc'),
+
+    url(r'^teacher_list/$', TeacherListView.as_view(), name='teacher_list'),
     # path('<int:org_id>/', OrgHomeView.as_view(), name='org_home'),
 ]
